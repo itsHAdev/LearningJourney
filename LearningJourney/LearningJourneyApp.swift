53d342fe22ef5b7ea11dfb72d0a36c0dfc268019
Hadeel <ha.ada1@hotmail.com>
2025-10-16 10:11:49 +0300

    Initial Commit

---
+++ LearningJourney/LearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyApp.swift
+//  LearningJourney
+//
+//  Created by Hadeel Alansari on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningJourneyTests/LearningJourneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyTests.swift
+//  LearningJourneyTests
+//
+//  Created by Hadeel Alansari on 16/10/2025.
+//
+
+import Testing
+@testable import LearningJourney
+
+struct LearningJourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
